e1d8449abd518819ee479c6607fa3de8f20dd890
ranuragpillai <46356136+ranuragpillai@users.noreply.github.com>
2019-03-20 20:45:08 +0530

    Add files via upload

---
+++ IFTTT_led_nodeMCU.ino
@@ -0,0 +1,83 @@
+#include<ESP8266WiFi.h>
+#include "Adafruit_MQTT_Client.h"
+
+// Wifi details
+#define wifi "internet here bitch, AMA"
+#define pass "dumdumdiggadigga"
+
+// adafruit details
+#define user "anurag403"
+#define key "aec4453133614cf99a6032a18ebebad9"
+
+#define port 1883
+#define server "io.adafruit.com"
+
+// pin_out for ldr
+#define led 16
+
+WiFiClient esp;
+Adafruit_MQTT_Client mqtt(&esp, server, port, user, key);
+Adafruit_MQTT_Subscribe feed =Adafruit_MQTT_Subscribe(&mqtt,user"/feeds/ldrprogram");
+
+void setup() {
+  pinMode(16,OUTPUT);
+  Serial.begin(115200);
+  delay(10);
+  Serial.println("Adafruit Demo");
+  Serial.print("Connecting to ");
+  Serial.print(wifi);
+
+  WiFi.begin(wifi,pass);
+  
+  while(WiFi.status()!= WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(" . ");
+  }
+  
+  Serial.println("Connected to WiFi");
+  Serial.print(wifi);
+  Serial.println("IP Address");
+  Serial.print(WiFi.localIP());
+  
+  mqtt.subscribe(&feed);
+  Serial.println("Connecting through MQTT");
+
+  while(mqtt.connect())
+  {
+    Serial.print(" . ");
+  }
+  Serial.println("Connected");
+ 
+}
+
+void loop() {
+  while(mqtt.readSubscription(3000))
+  {
+    Serial.print("Got : "); 
+    //Serial.println((char*)feed.lastread);
+    int a = atoi((char*)feed.lastread);
+    
+    Serial.print(a);
+    while(feed.lastread == 1 || feed.lastread ==
+    if (a==0)
+      digitalWrite(16,LOW);
+    else if (a==1)
+      digitalWrite(16,HIGH);
+    else
+    {
+      int i = 0;
+      while(i<20)
+      {
+        i=i+1;
+        digitalWrite(16,HIGH);
+        delay(100);
+        digitalWrite(16,LOW);
+        delay(100);
+        digitalWrite(16,HIGH);
+      }
+    }
+
+    delay(2200);
+  }
+}
